0f6de4ab0694e3ce8b8390cc304b3caa9afcfcd4
JpryHyrd <42839731+JpryHyrd@users.noreply.github.com>
2019-02-22 15:14:47 +0300

    gg
    gg

--- Django_DZ/Kazakov_Artur/server/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Program Files\1Google\Google Диск\project_HtmlCSS\jungo
-manage.py runserver
--- Django_DZ/Kazakov_Artur/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\program files\1google\google диск\project_htmlcss\jungo\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
